ecc2efecd4f8bcbd0a79cea9a55064f252475cca
Dai Ngo Tan <ngotandai95@gmail.com>
2021-07-05 19:18:09 -0400

    update

---
+++ JAVA INTRO/ClassesObjects_13/src/App.java
@@ -0,0 +1,31 @@
+class Person {
+
+    //Instance variables (data or "state")
+    String name;
+    int age;
+
+
+    // Classes can contain
+
+    // 1. Data
+    // 2. Subroutines (methods)
+}
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1.name = "Joe Bloggs";
+        person1.age = 37;
+
+        Person person2 = new Person();
+        person2.name = "Sarah Smith";
+        person2.age = 20;
+
+        System.out.println(person1.name);
+
+    }
+
+}
