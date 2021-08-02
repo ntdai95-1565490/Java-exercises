bbd1fa66d91d2acac193422d97dbc81b77f4d128
Dai Ngo Tan <ngotandai95@gmail.com>
2021-08-02 15:14:27 -0500

    update

---
+++ JAVA MC/CodingExercises/41_SortedArray/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = SortedArray.getIntegers(5);
+        int[] sorted = SortedArray.sortIntegers(array);
+        SortedArray.printArray(sorted);
+    }
+}
---
+++ JAVA MC/CodingExercises/43_ReverseArray/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ReverseArray.call(new int[] {1, 2, 3, 4, 5});
+    }
+}
