63631d1a404e9cef9196ee9af70aa67783102b9b
Dai Ngo Tan <ngotandai95@gmail.com>
2021-08-09 22:58:44 -0500

    update

---
+++ JAVA MC/Lectures/Threads/src/academy/learnprogramming/MyRunnable.java
@@ -0,0 +1,11 @@
+package academy.learnprogramming;
+
+import static academy.learnprogramming.ThreadColor.ANSI_RED;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
+    }
+}
